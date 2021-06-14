6f8f072b1fadd8943c1b836ec74cd451be4916df
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-06-15 00:39:04 +0530

    create an ip4 packet with only hdr

--- src/pmtud.rs
+++ src/pmtud.rs
@@ -1,8 +1,10 @@
 use crate::errors::PmtudError;
+use std::net::Ipv4Addr;
 use pnet::packet::{icmp::Icmp, ip::IpNextHeaderProtocols::Ipv4};
 use pnet::packet::ip::IpNextHeaderProtocol;
 use pnet::packet::ip::IpNextHeaderProtocols::Icmp;
 use pnet::packet::ipv4::MutableIpv4Packet;
+use pnet::packet::ipv4::checksum;
 use pnet::transport::{
     transport_channel, TransportChannelType, TransportReceiver, TransportSender,
 };
@@ -11,10 +13,11 @@ use pnet::packet::ipv4::Ipv4Flags::DontFragment;
 pub struct Pmtud {
     tx: TransportSender,
     rx: TransportReceiver,
+    destination: Ipv4Addr
 }
 
 impl Pmtud {
-    pub fn new() -> Result<Self, PmtudError> {
+    pub fn new(destination: Ipv4Addr) -> Result<Self, PmtudError> {
         let (tx, rx) = transport_channel(
             1500,
             TransportChannelType::Layer3(Ipv4),
@@ -23,14 +26,32 @@ impl Pmtud {
 
         Ok(Self {
             tx,
-            rx
+            rx,
+            destination
         })
     }
 
-    pub fn discover(&self) -> u8 {
-        // TODO abhi -
-        // construct an IPv4 packet with DF set
-
+    pub fn discover(&mut self) -> u8 {
+        let mut packet = [0u8; 20];
+        let mut packet = MutableIpv4Packet::new(&mut packet).unwrap();
+        packet.set_version(4);
+        packet.set_header_length(5);
+        packet.set_dscp(0); // standard diff service class
+        packet.set_ecn(1); // we support cogestion notification
+        packet.set_total_length(20); // only header
+        packet.set_identification(1);
+        packet.set_flags(DontFragment);
+        packet.set_fragment_offset(0);
+        packet.set_ttl(10); // in seconds
+        packet.set_next_level_protocol(Icmp);
+        packet.set_source("127.0.0.1".parse().unwrap()); // the nat can change this address
+        packet.set_destination(self.destination);
+        // the routers will recalc the checksum before forwarding since they decrease ttl by 1
+        packet.set_checksum(checksum(&packet.to_immutable())); 
+        match self.tx.send_to(packet, self.destination.into()) {
+            Err(e) => println!("there was a problem sending the ip packet to destination - {}", e),
+            _ => {}
+        }
         todo!()
     }
 }
