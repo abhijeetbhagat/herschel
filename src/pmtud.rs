d0c7012a1c959586bd78e7c26f516f04d69d9da1
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-06-16 23:31:54 +0530

    iterate over packets recvd

--- src/pmtud.rs
+++ src/pmtud.rs
@@ -1,33 +1,30 @@
 use crate::errors::PmtudError;
-use std::net::Ipv4Addr;
-use pnet::packet::{icmp::Icmp, ip::IpNextHeaderProtocols::Ipv4};
 use pnet::packet::ip::IpNextHeaderProtocol;
 use pnet::packet::ip::IpNextHeaderProtocols::Icmp;
-use pnet::packet::ipv4::MutableIpv4Packet;
 use pnet::packet::ipv4::checksum;
-use pnet::transport::{
-    transport_channel, TransportChannelType, TransportReceiver, TransportSender,
-};
 use pnet::packet::ipv4::Ipv4Flags::DontFragment;
+use pnet::packet::ipv4::MutableIpv4Packet;
+use pnet::packet::{icmp::Icmp, ip::IpNextHeaderProtocols::Ipv4};
+use pnet::transport::{
+    ipv4_packet_iter, transport_channel, TransportChannelType, TransportReceiver, TransportSender,
+};
+use std::net::Ipv4Addr;
 
 pub struct Pmtud {
     tx: TransportSender,
     rx: TransportReceiver,
-    destination: Ipv4Addr
+    destination: Ipv4Addr,
 }
 
 impl Pmtud {
     pub fn new(destination: Ipv4Addr) -> Result<Self, PmtudError> {
-        let (tx, rx) = transport_channel(
-            1500,
-            TransportChannelType::Layer3(Ipv4),
-        )
-        .map_err(|e| PmtudError::PmtudLayer3TransportInitError)?;
+        let (tx, rx) = transport_channel(1500, TransportChannelType::Layer3(Ipv4))
+            .map_err(|e| PmtudError::PmtudLayer3TransportInitError)?;
 
         Ok(Self {
             tx,
             rx,
-            destination
+            destination,
         })
     }
 
@@ -44,13 +41,16 @@ impl Pmtud {
         packet.set_fragment_offset(0);
         packet.set_ttl(10); // in seconds
         packet.set_next_level_protocol(Icmp);
-        packet.set_source("127.0.0.1".parse().unwrap()); // the nat can change this address
+        packet.set_source("127.0.0.1".parse().unwrap()); // nats can change this address
         packet.set_destination(self.destination);
         // the routers will recalc the checksum before forwarding since they decrease ttl by 1
-        packet.set_checksum(checksum(&packet.to_immutable())); 
+        packet.set_checksum(checksum(&packet.to_immutable()));
         match self.tx.send_to(packet, self.destination.into()) {
-            Err(e) => println!("there was a problem sending the ip packet to destination - {}", e),
-            _ => {}
+            Err(e) => println!(
+                "there was a problem sending the ip packet to destination - {}",
+                e
+            ),
+            _ => while let Ok((packet, addr)) = ipv4_packet_iter(&mut self.rx).next() {},
         }
         todo!()
     }
