c6ca7bdc0adbeab9481be18db8457255c845c0d0
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-06-21 16:05:42 +0530

    add thiserror crate for better error handling

--- src/errors.rs
+++ src/errors.rs
@@ -4,4 +4,6 @@ use thiserror::Error;
 pub enum PmtudError {
     #[error("error setting up an association: {0}")]
     PmtudLayer3TransportInitError(String),
+    #[error("error recving response")]
+    PmtudRecvError,
 }
